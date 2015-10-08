39ca89d6cb4ed4b4213d9f2c3aa0a59a31ee1545
Mike Ligthart <mike.ligthart@gmail.com>
2015-10-08 18:16:14 +0200

    build photo app basic

--- app/src/main/java/mypal/tno/nl/mypalphoto/MainActivity.java
+++ app/src/main/java/mypal/tno/nl/mypalphoto/MainActivity.java
@@ -6,11 +6,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -18,9 +18,21 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -31,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
 
-    String mCurrentPhotoPath;
+    private String mCurrentPhotoPath;
     private File photoFile = null;
     private FloatingActionButton fab;
     private ImageView mImageView;
@@ -91,6 +103,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    /* PRIVATE METHODS */
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -112,9 +125,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    /**
-     * http://developer.android.com/training/camera/photobasics.html
-     */
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -164,13 +174,122 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mImageView.setImageBitmap(rotatedBMP);
 
-        /**
         try {
             sendPhoto(rotatedBMP);
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-         **/
+    }
+
+    private void sendPhoto(Bitmap bitmap) throws Exception {
+        new UploadTask().execute(bitmap);
+    }
+
+    private class UploadTask extends AsyncTask<Bitmap, Void, Void> {
+
+        private String attachmentName = "myPALPhoto";
+        private String attachmentFileName = "myPALPhoto.png";
+        private String crlf = "\r\n";
+        private String twoHyphens = "--";
+        private String boundary =  "*****";
+        private String server = "http://192.168.178.14:9000/dropphoto";
+
+        protected Void doInBackground(Bitmap... bitmaps) {
+            if (bitmaps[0] == null)
+                return null;
+            setProgress(0);
+
+            HttpURLConnection httpUrlConnection = null;
+            try {
+                URL url = new URL(server);
+                httpUrlConnection = (HttpURLConnection) url.openConnection();
+                httpUrlConnection.setUseCaches(false);
+                httpUrlConnection.setDoOutput(true);
+                httpUrlConnection.setRequestMethod("POST");
+                httpUrlConnection.setRequestProperty("Connection", "Keep-Alive");
+                httpUrlConnection.setRequestProperty("Cache-Control", "no-cache");
+                httpUrlConnection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + this.boundary);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            DataOutputStream request = null;
+            try {
+                request = new DataOutputStream(httpUrlConnection.getOutputStream());
+                request.writeBytes(this.twoHyphens + this.boundary + this.crlf);
+                request.writeBytes("Content-Disposition: form-data; name=\"" + this.attachmentName + "\";filename=\"" + this.attachmentFileName + "\"" + this.crlf);
+                request.writeBytes(this.crlf);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Bitmap bitmap = bitmaps[0];
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream); // convert Bitmap to ByteArrayOutputStream
+            //InputStream in = new ByteArrayInputStream(stream.toByteArray()); // convert ByteArrayOutputStream to ByteArrayInputStream
+
+            try {
+                request.write(stream.toByteArray());
+                request.writeBytes(this.crlf);
+                request.writeBytes(this.twoHyphens + this.boundary + this.twoHyphens + this.crlf);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                request.flush();
+                request.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            InputStream responseStream = null;
+            try {
+                responseStream = new BufferedInputStream(httpUrlConnection.getInputStream());
+                BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+                String line = "";
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((line = responseStreamReader.readLine()) != null)
+                {
+                    stringBuilder.append(line).append("\n");
+                }
+                responseStreamReader.close();
+
+                Log.i("TAG", stringBuilder.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                responseStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "UploadTask IOException while closing stream: " + e.getLocalizedMessage());
+                }
+            }
+
+            httpUrlConnection.disconnect();
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            Toast.makeText(MainActivity.this, R.string.uploaded, Toast.LENGTH_LONG).show();
+        }
     }
 }
